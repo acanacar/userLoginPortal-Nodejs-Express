6cdbc4f579bfc6556cf2e53b66ac7782afd60229
acanacar <ahmetcanacar.1@gmail.com>
2019-02-19 23:00:09 +0300

    first commit

---
+++ routes/users.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
+
+//https://dev-135388.oktapreview.com
+
+//0oajfp0izwa0PMIqD0h7
+
+//apl_c6PQBYM0YKjFLm0iMfZArKxmVqnjPVauliVz
+
+//009ngOv6Ew95zhgwRMXuR-31_wE1UKpTv_wiKkEqdd
